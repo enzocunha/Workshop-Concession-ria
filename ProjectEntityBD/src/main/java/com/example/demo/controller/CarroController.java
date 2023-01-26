c26adff040d1edf752691782d835533105cf35da
enzocunha <enzcdn@discente.ufg.br>
2023-01-26 11:38:57 -0300

    exemplo do professor refatorado de pessoa para carro

---
+++ ProjectEntityBD/src/main/java/com/example/demo/controller/CarroController.java
@@ -0,0 +1,77 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Carro;
+import com.example.demo.repository.CarroRepository;
+
+// A anotação @RestController permite definir um controller com características API REST;
+@RestController
+public class CarroController {
+	// A anotação @Autowired delega ao Spring Boot a inicialização do objeto;
+    @Autowired
+    private CarroRepository carroRepository;
+    // A anotação @RequestMapping permite definir uma rota
+    @RequestMapping(value = "/carro", method = RequestMethod.GET)
+    public List<Carro> Get() {
+        return carroRepository.findAll();
+    }
+    
+    // @PathVariable indica que o valor da variável virá de uma informação da rota;
+    @RequestMapping(value = "/carro/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Carro> GetById(@PathVariable(value = "id") long id)
+    {
+    	// https://docs.oracle.com/javase/9/docs/api/java/util/Optional.html (desde v 1.8)
+    	// findById espera um retorno do tipo Optional<Pessoa>
+        Optional<Carro> carro = carroRepository.findById(id);
+        if(carro.isPresent())
+            return new ResponseEntity<Carro>(carro.get(), HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    
+    // @RequestBody indica que o valor do objeto virá do corpo da requisição e 
+    //              consegue mapear os dados vindos em Json para os atributos da classe;
+    @RequestMapping(value = "/carro", method =  RequestMethod.POST)
+    public Carro Post(@RequestBody Carro carro)
+    {
+        return carroRepository.save(carro);
+    }    
+    
+    @RequestMapping(value = "/carro/{id}", method =  RequestMethod.PUT)
+    public ResponseEntity<Carro> Put(@PathVariable(value = "id") long id, @RequestBody Carro newCarro)
+    {
+        Optional<Carro> oldCarro = carroRepository.findById(id);
+        if(oldCarro.isPresent()){
+            Carro carro = oldCarro.get();
+            carro.setNome(newCarro.getNome());
+            carroRepository.save(carro);
+            return new ResponseEntity<Carro>(carro, HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }    
+    
+    @RequestMapping(value = "/carro/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> Delete(@PathVariable(value = "id") long id)
+    {
+        Optional<Carro> carro = carroRepository.findById(id);
+        if(carro.isPresent()){
+            carroRepository.delete(carro.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }    
+    
+}	
