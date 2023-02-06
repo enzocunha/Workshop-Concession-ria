15b7ed3d4f2c9017d305728b5fcde1441f2fe052
gabriel-freitas-melo <48661085+gabriel-freitas-melo@users.noreply.github.com>
2023-02-06 10:48:20 -0300

    Delete ClienteRepository.java

--- ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.demo.entity.Cliente;
-
-/* aqui basta fazer o interface*/
-@Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> { }
